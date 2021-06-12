9b5c77b952e95ab45f6eace11ce8a40e872fdbc2
zahid-bracu <zahid.mym@gmail.com>
2021-06-13 01:28:09 +0600

    ok

---
+++ freeCodeCamp/application/src/Component/UseEffect/UseEffectMultipleReturns.js
@@ -0,0 +1,55 @@
+import React, {useState , useEffect} from 'react';
+
+const UseEffectMultipleReturns = () => {
+ const url='https://api.github.com/users/mojombo';
+ const [isLoading,setLoading]=useState(true);
+ const [isError,setError]=useState(false)
+ const [user,setUser]=useState('default user')
+
+ function getUser(){
+  fetch(url).then(response=>{
+   if(response.status>=200 & response.status<=299){
+    return response.json()
+   }else if(response.status>=300){
+    setError(true);
+    setLoading(false);
+   }
+  })
+  .then(result=>{
+   console.log(result);
+   const {login}=result;
+   setLoading(false);
+   setUser(login);
+  })
+  .catch(err=>{
+   
+   console.log(err)
+  })
+ }
+
+
+ useEffect(()=>{
+  getUser();
+ },[])
+
+ console.log(user);
+
+if(isLoading){
+ return(
+  <h3>Is Loading</h3>
+ )
+}
+if(isError){
+ return(
+  <h2>Network Error</h2>
+ )
+
+}else if(user){
+ return(
+  <h2>{user}</h2>
+ )
+}
+ 
+}
+
+export default UseEffectMultipleReturns;
