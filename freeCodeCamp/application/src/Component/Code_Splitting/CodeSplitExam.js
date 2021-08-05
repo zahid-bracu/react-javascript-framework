9ce2607430cffe8cb0029c5c2ea5b603b93d1d2e
zahid-bracu <zahid.mym@gmail.com>
2021-08-05 20:51:48 +0600

    'ok'

--- freeCodeCamp/application/src/Component/Code_Splitting/CodeSplitExam.js
+++ freeCodeCamp/application/src/Component/Code_Splitting/CodeSplitExam.js
@@ -3,10 +3,14 @@ import React, {useState} from 'react'
 
 export default function CodeSplitExam() {
     const [state, setstate] = useState(0)
+    
+    // importing splitted code
     import("./File").then(res => {
         const sum=res.add(16, 26);
         setstate(sum);
       });
+
+
     return (
         <div>
             <h1>Value : {state}</h1>
